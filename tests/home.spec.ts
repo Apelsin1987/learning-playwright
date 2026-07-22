4ac58b655b7fe2036883aec525f50ca56c33b64c
Konstantin Si <apelsin1987@gmail.com>
2026-07-22 21:07:04 +0200

    refactor: home tests

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,24 +1,39 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Locator } from '@playwright/test';
 
-test("Home page", async ({ page }) => {
-  await page.goto("/");
+test.describe('Home page', () => {
 
-  // Ensure the sign-in link is present
-  await expect(page.getByTestId('nav-sign-in')).toHaveText('Sign in');
+  let productGrid: Locator;
 
-  // Check the title of the page
-  await expect(page).toHaveTitle('Practice Software Testing - Toolshop - v5.0');
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+    productGrid = page.locator('.col-md-9');
+  });
 
-  // Check the count of items displayed
-  const productGrid = await page.locator('.col-md-9');
-  await expect(productGrid.getByRole('link')).toHaveCount(9);
+  test("check sign in", async ({ page }) => {
+    // Ensure the sign-in link is present
+    await expect(page.getByTestId('nav-sign-in')).toHaveText('Sign in');
+  });
 
-  // Search for Thor Hammer and check that the result is one, have search text and visible
-  await page.getByTestId('search-query').fill('Thor Hammer');
-  await page.getByTestId('search-submit').click();
-  await expect(productGrid.getByRole('link')).toHaveCount(1);
+  test("validate page title", async ({ page }) => {
+    // Check the title of the page
+    await expect(page).toHaveTitle('Practice Software Testing - Toolshop - v5.0');
+  });
 
-  const searchResult = await productGrid.getByRole('link').first();
-  await expect(searchResult).toBeVisible();
-  await expect(searchResult).toHaveText(/Thor Hammer/);
+  test("grid loads with 9 items", async ({ page }) => {
+    // Check the count of items displayed
+    await productGrid.getByRole('link').first().waitFor({ state: 'visible' });
+    await expect(productGrid.getByRole('link')).toHaveCount(9);
+  });
+
+  test("search for Thor Hammer", async ({ page }) => {
+    // Search for Thor Hammer and check that the result is one, have search text and visible
+    await page.getByTestId('search-query').fill('Thor Hammer');
+    await page.getByTestId('search-submit').click();
+    await productGrid.getByRole('link').first().waitFor({ state: 'visible' });
+    await expect(productGrid.getByRole('link')).toHaveCount(1);
+
+    const searchResult = await productGrid.getByRole('link').first();
+    await expect(searchResult).toBeVisible();
+    await expect(searchResult).toHaveText(/Thor Hammer/);
+  });
 });
