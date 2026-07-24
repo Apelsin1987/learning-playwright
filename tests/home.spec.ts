69773387467f2ea4d8ad424df77cc722c70b737c
Konstantin Si <apelsin1987@gmail.com>
2026-07-24 16:03:32 +0200

    feat: add order checkout test

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -40,7 +40,7 @@ test.describe('Home page with no auth', () => {
     await productGrid.getByRole('link').first().waitFor({ state: 'visible' });
     await expect(productGrid.getByRole('link')).toHaveCount(1);
 
-    const searchResult = await productGrid.getByRole('link').first();
+    const searchResult = productGrid.getByRole('link').first();
     await expect(searchResult).toBeVisible();
     await expect(searchResult).toHaveText(/Thor Hammer/);
   });
@@ -67,6 +67,46 @@ test.describe('Home page customer 01 auth', () => {
   test("check customer 01 is signed in", async ({ page }) => {
     await expect(page.getByTestId('nav-sign-in')).not.toBeVisible();
     await expect(page.getByTestId('nav-menu')).toContainText('Jane Doe');
+  });
+
+  test("order checkout", async ({ page }) => {
+
+    const productGrid = page.locator('.col-md-9');
+    await productGrid.getByRole('link').first().click();
+
+    await expect(page.getByTestId('product-name')).toBeVisible();
+    const productName = await page.getByTestId('product-name').textContent();
+
+    await page.getByTestId('increase-quantity').click();
+    await expect(page.getByTestId('quantity')).toHaveValue('2');
+
+    await page.getByTestId('add-to-cart').click();
+    await page.getByTestId('nav-cart').click();
+
+    if (productName !== null) {
+      await expect(page.getByTestId('product-title')).toHaveText(productName);
+    }
+
+    await page.getByTestId('proceed-1').click();
+
+    await expect(page.getByTestId('proceed-2')).toBeVisible();
+    await page.getByTestId('proceed-2').click();
+
+    await page.getByTestId('postal_code').fill('12345');
+    await page.getByTestId('house_number').fill('123');
+    await expect(page.getByTestId('proceed-3')).toBeEnabled();
+    await page.getByTestId('proceed-3').click();
+
+    await expect(page.getByTestId('payment-method')).toBeVisible();
+    await page.getByTestId('payment-method').selectOption('buy-now-pay-later');
+    await expect(page.getByTestId('monthly_installments')).toBeVisible();
+    await page.getByTestId('monthly_installments').selectOption('3');
+    await expect(page.getByTestId('finish')).toBeEnabled();
+    await page.getByTestId('finish').click();
+
+    await expect(page.getByTestId('payment-success-message')).toBeVisible();
+
+    await expect(page).toHaveScreenshot('order-checkout-customer01.png');
   });
 
 });
