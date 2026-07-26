9b55abb784e4201f6176f296ff22e5b6a0c46c7a
Konstantin Si <apelsin1987@gmail.com>
2026-07-26 12:09:12 +0200

    feat: add login.page and its usage in auth test

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,6 +1,7 @@
 import { mkdirSync } from "fs";
 import { resolve } from "path";
 import { test as setup, expect } from "@playwright/test";
+import { LoginPage } from "../lib/pages/login.page";
 
 setup("Create customer 01 auth", async ({ page, context }) => {
   const email = 'customer@practicesoftwaretesting.com';
@@ -8,15 +9,13 @@ setup("Create customer 01 auth", async ({ page, context }) => {
 
   const authDir = resolve(__dirname, "..", "auth");
   mkdirSync(authDir, { recursive: true });
-
   const customer01AuthFile = resolve(authDir, "customer01.json");
 
-  await page.goto('/auth/login');
-
-  await page.getByTestId('email').fill(email);
-  await page.getByTestId('password').fill(password);
-  await page.getByTestId('login-submit').click();
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.login(email, password);
 
   await expect(page.getByTestId('nav-menu')).toContainText('Jane Doe');
+
   await context.storageState({ path: customer01AuthFile });
 });
